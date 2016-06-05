2c7ce2dbf214505f8f42fb9bbffbebcb6e310af2
matsuhisa <matsuhisa+github@gmail.com>
2016-06-05 23:41:42 +0900

    Add spec

--- spec/wango_spec.rb
+++ spec/wango_spec.rb
@@ -4,4 +4,14 @@ describe Wango do
   it 'has a version number' do
     expect(Wango::VERSION).not_to be nil
   end
+
+  WORDS = [
+    ["こんにちは", "こんにちはワン :dog:"]
+  ]
+
+  it 'String#wango' do
+    WORDS.each do |word|
+      expect(word[0].wango).to eq word[1]
+    end
+  end
 end
